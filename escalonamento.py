6fd113dc734f1e49e5b4e92932030b3a2bf34638
Rodrigo Araújo de Souza <86501488+RodrigoAraujo12@users.noreply.github.com>
2025-02-16 19:53:05 -0300

    Update escalonamento.py
    Implementação do SJF

--- escalonamento.py
+++ escalonamento.py
@@ -12,7 +12,7 @@ def ler_arquivo(nome_arquivo):
     
     return tempoChegadas, tempoPicos
 
-def imprimir_tabela_final(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista):
+def imprimir_tabela_FCFS(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista):
     quantidadeProcessos = len(tempoChegadas)
     
     for i in range(quantidadeProcessos):
@@ -21,7 +21,18 @@ def imprimir_tabela_final(tempoChegadas, tempoEsperaLista, tempoRespostaLista, t
         tempoMedioResposta = sum(tempoRespostaLista) / quantidadeProcessos
     
 
-    print("FCFCS: {:.1f} {:.1f} {:.1f}".format(tempoMedioRetorno, tempoMedioResposta, tempoMedioEspera))
+    print("FCFS: {:.1f} {:.1f} {:.1f}".format(tempoMedioRetorno, tempoMedioResposta, tempoMedioEspera))
+
+def imprimir_tabela_SJF(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista):
+    quantidadeProcessos = len(tempoChegadas)
+    
+    for i in range(quantidadeProcessos):
+        tempoMedioEspera = sum(tempoEsperaLista) / quantidadeProcessos
+        tempoMedioRetorno = sum(tempoRetornoLista) / quantidadeProcessos
+        tempoMedioResposta = sum(tempoRespostaLista) / quantidadeProcessos
+    
+
+    print("SJF: {:.1f} {:.1f} {:.1f}".format(tempoMedioRetorno, tempoMedioResposta, tempoMedioEspera))
 
 def FCFS(tempoChegadas, tempoPicos):
     quantidadeProcessos = len(tempoChegadas)
@@ -60,10 +71,53 @@ def FCFS(tempoChegadas, tempoPicos):
 
 
     # Chama a função que imprime os resultados
-    imprimir_tabela_final(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista)
+    imprimir_tabela_FCFS(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista)
+
+def SJF(tempos_chegada, picos_cpu):
+    
+    processos = list(zip(tempos_chegada, picos_cpu))
+    processos.sort(key=lambda x: (x[0], x[1]))  # Ordena por tempo de chegada e depois por tempo de execução
+    
+    schedule = []  # Lista de tempos de início
+    tempo_atual = 0  # Tempo atual do sistema
+    fila = []  # Fila de processos disponíveis
+    indice = 0  # Índice para percorrer a lista de processos
+    num_processos = len(processos)
+    
+    tempos_retorno = []
+    tempos_resposta = []
+    tempos_espera = []
+    
+    while indice < num_processos or fila:
+        # Adicionar processos à fila quando chegarem
+        while indice < num_processos and processos[indice][0] <= tempo_atual:
+            fila.append(processos[indice])
+            indice += 1
+        
+        if fila:
+            fila.sort(key=lambda x: x[1])  # Ordena por menor tempo de execução
+            proximo_processo = fila.pop(0)  # Pega o processo com menor tempo de execução
+            tempo_inicio = tempo_atual  # Momento em que o processo começa a execução
+            tempo_termino = tempo_inicio + proximo_processo[1]
+            
+            schedule.append((proximo_processo[0], tempo_inicio))  # Registra o início
+            tempo_atual = tempo_termino  # Atualiza o tempo atual
+            
+            # Cálculo das métricas
+            tempo_retorno = tempo_termino - proximo_processo[0]
+            tempo_resposta = tempo_inicio - proximo_processo[0]
+            tempo_espera = tempo_resposta  # Para SJF sem preempção, espera = resposta
+            
+            tempos_retorno.append(tempo_retorno)
+            tempos_resposta.append(tempo_resposta)
+            tempos_espera.append(tempo_espera)
+        else:
+            tempo_atual = processos[indice][0]  # Avança para o próximo processo se a fila estiver vazia
+    
+    imprimir_tabela_SJF(tempos_chegada, tempos_espera, tempos_resposta, tempos_retorno)
 
 arquivo = "testes.txt"
 tempoChegadas, tempoPicos = ler_arquivo(arquivo)
 FCFS(tempoChegadas, tempoPicos)
-
+SJF(tempoChegadas, tempoPicos)
 
