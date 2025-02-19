ea141aa729a652d2a2bf828f15a35e92f1210dda
João Marcelo Candido Borges <62162035+jmcandido@users.noreply.github.com>
2025-02-19 15:11:45 +0000

    melhorando sjf e impressao dos resultados

--- escalonamento.py
+++ escalonamento.py
@@ -1,6 +1,5 @@
 def ler_arquivo(nome_arquivo):
 
-    """Lê os tempos de chegada e execução do arquivo."""
     tempoChegadas = []
     tempoPicos = []
     
@@ -12,36 +11,12 @@ def ler_arquivo(nome_arquivo):
     
     return tempoChegadas, tempoPicos
 
-def imprimir_tabela_FCFS(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista):
-    quantidadeProcessos = len(tempoChegadas)
-    
-    for i in range(quantidadeProcessos):
-        tempoMedioEspera = sum(tempoEsperaLista) / quantidadeProcessos
-        tempoMedioRetorno = sum(tempoRetornoLista) / quantidadeProcessos
-        tempoMedioResposta = sum(tempoRespostaLista) / quantidadeProcessos
-    
-
-    print("FCFS: {:.1f} {:.1f} {:.1f}".format(tempoMedioRetorno, tempoMedioResposta, tempoMedioEspera))
-
-def imprimir_tabela_SJF(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista):
-    quantidadeProcessos = len(tempoChegadas)
-    
-    for i in range(quantidadeProcessos):
-        tempoMedioEspera = sum(tempoEsperaLista) / quantidadeProcessos
-        tempoMedioRetorno = sum(tempoRetornoLista) / quantidadeProcessos
-        tempoMedioResposta = sum(tempoRespostaLista) / quantidadeProcessos
-    
-
-    print("SJF: {:.1f} {:.1f} {:.1f}".format(tempoMedioRetorno, tempoMedioResposta, tempoMedioEspera))
-
 def FCFS(tempoChegadas, tempoPicos):
     quantidadeProcessos = len(tempoChegadas)
-    
     processos = list(zip(tempoChegadas, tempoPicos))
     
     # Ordena os processos por tempo de chegada (caso não estejam ordenados)
     processos.sort(key=lambda x: x[0])
-
 
     tempoAtual = 0
     inicioExecucao = []
@@ -63,7 +38,8 @@ def FCFS(tempoChegadas, tempoPicos):
     # Calculando tempos individuais
     for i in range(quantidadeProcessos):
         tempoRetorno = fimExecucao[i] - tempoChegadas[i]
-        tempoResposta = tempoEspera = inicioExecucao[i] - tempoChegadas[i]
+        tempoEspera = tempoRetorno - tempoPicos[i]
+        tempoResposta = inicioExecucao[i] - tempoChegadas[i]
         
         tempoRetornoLista.append(tempoRetorno)
         tempoRespostaLista.append(tempoResposta)
@@ -71,53 +47,76 @@ def FCFS(tempoChegadas, tempoPicos):
 
 
     # Chama a função que imprime os resultados
-    imprimir_tabela_FCFS(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista)
+    imprimir_tabela_final(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista,0)
 
-def SJF(tempos_chegada, picos_cpu):
+def SJF(tempoChegadas, tempoPicos):
     
-    processos = list(zip(tempos_chegada, picos_cpu))
-    processos.sort(key=lambda x: (x[0], x[1]))  # Ordena por tempo de chegada e depois por tempo de execução
+    quantidadeProcessos = len(tempoChegadas)
+    processos = list(zip(tempoChegadas, tempoPicos))
     
-    schedule = []  # Lista de tempos de início
-    tempo_atual = 0  # Tempo atual do sistema
-    fila = []  # Fila de processos disponíveis
-    indice = 0  # Índice para percorrer a lista de processos
-    num_processos = len(processos)
-    
-    tempos_retorno = []
-    tempos_resposta = []
-    tempos_espera = []
-    
-    while indice < num_processos or fila:
-        # Adicionar processos à fila quando chegarem
-        while indice < num_processos and processos[indice][0] <= tempo_atual:
-            fila.append(processos[indice])
-            indice += 1
+    # Ordena inicialmente os processos pelo tempo de chegada
+    processos.sort(key=lambda x: x[0])
+
+    tempoAtual = 0
+    inicioExecucao = []
+    fimExecucao = []
+    tempoEsperaLista = []
+    tempoRetornoLista = []
+    tempoRespostaLista = []
+
+    processos_prontos = []
+    processos_restantes = processos.copy()
+
+    while processos_restantes or processos_prontos:
+        # Adicionar à fila de prontos os processos que já chegaram
+        while processos_restantes and processos_restantes[0][0] <= tempoAtual:
+            processos_prontos.append(processos_restantes.pop(0))
+
+        # Se não há processos prontos, avançar no tempo
+        if not (processos_prontos):
+            tempoAtual = processos_restantes[0][0]
+            continue
+
+        # Escolher o processo com o menor tempo de execução
+        processos_prontos.sort(key=lambda x: x[1])
+        chegada, pico = processos_prontos.pop(0)
+
+        inicioExecucao.append(tempoAtual)
+        tempoAtual += pico
+        fimExecucao.append(tempoAtual)
         
-        if fila:
-            fila.sort(key=lambda x: x[1])  # Ordena por menor tempo de execução
-            proximo_processo = fila.pop(0)  # Pega o processo com menor tempo de execução
-            tempo_inicio = tempo_atual  # Momento em que o processo começa a execução
-            tempo_termino = tempo_inicio + proximo_processo[1]
-            
-            schedule.append((proximo_processo[0], tempo_inicio))  # Registra o início
-            tempo_atual = tempo_termino  # Atualiza o tempo atual
-            
-            # Cálculo das métricas
-            tempo_retorno = tempo_termino - proximo_processo[0]
-            tempo_resposta = tempo_inicio - proximo_processo[0]
-            tempo_espera = tempo_resposta  # Para SJF sem preempção, espera = resposta
-            
-            tempos_retorno.append(tempo_retorno)
-            tempos_resposta.append(tempo_resposta)
-            tempos_espera.append(tempo_espera)
-        else:
-            tempo_atual = processos[indice][0]  # Avança para o próximo processo se a fila estiver vazia
+    # Calculando tempos individuais
+    for i in range(quantidadeProcessos):
+        tempoRetorno = fimExecucao[i] - tempoChegadas[i]
+        tempoEspera = tempoRetorno - tempoPicos[i]
+        tempoResposta = inicioExecucao[i] - tempoChegadas[i]
+        
+        tempoRetornoLista.append(tempoRetorno)
+        tempoRespostaLista.append(tempoResposta)
+        tempoEsperaLista.append(tempoEspera)
+
+    # Função de impressão de tabela
+    imprimir_tabela_final(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista, 1)
+
+def imprimir_tabela_final(tempoChegadas, tempoEsperaLista, tempoRespostaLista, tempoRetornoLista, id):
+    quantidadeProcessos = len(tempoChegadas)
     
-    imprimir_tabela_SJF(tempos_chegada, tempos_espera, tempos_resposta, tempos_retorno)
+    for i in range(quantidadeProcessos):
+        tempoMedioEspera = sum(tempoEsperaLista) / quantidadeProcessos
+        tempoMedioRetorno = sum(tempoRetornoLista) / quantidadeProcessos
+        tempoMedioResposta = sum(tempoRespostaLista) / quantidadeProcessos
+
+    
+    if (id == 0):
+         print("FCFS: {:.1f} {:.1f} {:.1f}".format(tempoMedioRetorno, tempoMedioResposta, tempoMedioEspera))
+    elif(id == 1):
+        print("SJF: {:.1f} {:.1f} {:.1f}".format(tempoMedioRetorno, tempoMedioResposta, tempoMedioEspera))
+
 
 arquivo = "testes.txt"
 tempoChegadas, tempoPicos = ler_arquivo(arquivo)
 FCFS(tempoChegadas, tempoPicos)
-SJF(tempoChegadas, tempoPicos)
+SJF(tempoChegadas,tempoPicos)
+
+
 
